88fb621dde1c7cbdcd86c6d1818a148eadc7b261
poliHorser <134537683+poliHorser@users.noreply.github.com>
2024-03-26 11:07:31 +0200

    finish

--- db/contacts.js
+++ db/contacts.js
@@ -1,40 +1,32 @@
-import fs from "fs/promises";
-import path from "path";
-import { nanoid } from "nanoid";
-
-const contactsPath = path.join('contacts', 'contacts.json');
+const fs = require('fs/promises');
+const path = require('path');
+const { nanoid } = require('nanoid');
+const contactsPath = path.join(__dirname, 'contacts.json');
+const updateContacts = contacts => fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
 async function listContacts() {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
-    return JSON.parse(data);
-  } catch (error) {
-    throw error;
-  }
+  const data = await fs.readFile(contactsPath);
+
+  return JSON.parse(data);
 }
 
-async function getContactById(contactId) {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
-    const contacts = JSON.parse(data);
-    return contacts.find(contact => contact.id === contactId) || null;
-  } catch (error) {
-    throw error;
-  }
+async function getContactById(id) {
+  const contacts = await listContacts();
+  const result = contacts.find(item => item.id === id);
+
+  return result || null;
 }
 
-async function removeContact(contactId) {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
-    const contacts = JSON.parse(data);
-    const index = contacts.findIndex(contact => contact.id === contactId);
-    if (index === -1) return null;
-    const removedContact = contacts.splice(index, 1)[0];
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return removedContact;
-  } catch (error) {
-    throw error;
-  }
+async function removeContact(id) {
+  const contacts = await listContacts();
+    const index = contacts.findIndex(item => item.id === id);
+    if (index === -1) {
+        return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await updateContacts(contacts);
+
+    return result;
 }
 
 async function addContact(name, email, phone) {
@@ -49,5 +41,4 @@ async function addContact(name, email, phone) {
     throw error;
   }
 }
-
 module.exports = { listContacts, getContactById, removeContact, addContact };
